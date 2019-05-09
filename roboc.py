fddee0e16b3b8d26a4fddc282b98aa6f0be5cd18
athd33 <antoinecours@gmx.fr>
2019-05-09 13:53:20 +0200

    Init ok, save a créer

--- roboc.py
+++ roboc.py
@@ -14,14 +14,11 @@ from carte import Carte
 cartes = []
 
 
-def myprint(mess):
+def myprint(mess):          #### fonction print avec sleep
     time.sleep(1)
     print(mess)
 
-
-
-
-for nom_fichier in os.listdir("cartes"):
+for nom_fichier in os.listdir("cartes"):        ###### recherche de fichiers dans le dossier cartes
     if nom_fichier.endswith(".txt"):
         chemin = os.path.join("cartes", nom_fichier)
         nom_carte = nom_fichier[:-3].lower()
@@ -31,7 +28,35 @@ for nom_fichier in os.listdir("cartes"):
 
 
 myprint("Bienvenu au jeu du labirynthe :")
+name = input("Quel est votre nom? : ")
+name = name.lower()
+
+
+def init_score():
+    print("Initialisation du score en cours")
+    try:
+        with open("data", "rb") as fichier:
+            record = pickle.Unpickler(fichier)
+            score = record.load()
+    except:
+        score = {}
+        print("Aucun fichier data trouvé")
+        print("Data vient d'être créé")
+    return score
+
+
+init_score()
+############ VERIFIER SI NOM DANS SCORE 
+
+
+
+myprint(f"Ok {name}, on va pouvoir commencer !")
+
+
+
 myprint("\nVoici les cartes disponibles :")
+
+
 
 # On affiche les cartes existantes
 for nom_fichier in os.listdir("cartes"):
