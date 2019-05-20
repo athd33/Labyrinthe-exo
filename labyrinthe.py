5331ded290a61716f08a5140043b1fd6e8377823
athd33 <antoinecours@gmx.fr>
2019-05-20 13:40:51 +0200

    Robot sous forme de liste, changement possible

--- labyrinthe.py
+++ labyrinthe.py
@@ -12,22 +12,35 @@ class Labyrinthe:
     def __init__(self, chaine):
         self.chaine = chaine
         content = self.chaine.split("\n")
-
                                         #création de l'imbrication de liste
         for x in content:
             ligne.append(list(x))
         for e in ligne:
             labyrinthe.append(e)
 
-                                        # indexation des éléments dans les listes
+      
+    def __repr__(self):                 # m"thode pour afficher le labyrinthe en cours
+        return f"\n{self.chaine}"
+
+
+
+    def deplacement(self):
+                                                      # indexation des éléments dans les listes 
+                                                    
+        robot = []
+        mur = []
+        porte = []
+        
         for index_x, x in enumerate(labyrinthe):
             for index_y, y in enumerate(x):
                 if y == "X":
                     robot = (index_x, index_y)
+                if y == "O":
+                    mur = (index_x, index_y)
+                if y == ".":
+                    porte = (index_x, index_y)
 
-    def __repr__(self):
-
-        return f"\n{self.chaine}"
+        print(robot)                            # retourne la position de robot
  
 
 
