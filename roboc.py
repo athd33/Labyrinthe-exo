d0fdd921fde554e9fbd6671a5922332e56427ee2
athd33 <antoinecours@gmx.fr>
2019-05-28 18:50:41 +0200

    update

--- roboc.py
+++ roboc.py
@@ -19,24 +19,26 @@ class Robot:
     def __init__(self, chaine ):
         self.chaine = chaine
         self.robot = []
-        self.obstacles = []
-        self.portes = []
+        obstacles = []
+        portes = []
         self.sortie = []
+        self.grille = []
+        
         for index_x, x in enumerate(self.chaine):
             for index_y, y in enumerate(x):
                 if y == "X":
                     self.robot = [index_x, index_y]        # récupération de la position de robot dans une liste
+                    self.grille += "X"
                 elif y == "U":
                     self.sortie = [index_x, index_y]     # récupération de la position de la sortie dans une liste
                 elif y == ".":
-                    self.porte = [index_x, index_y]      # récupération de la position des portes dans une liste
-                    self.portes.append(self.porte)
+                    porte = [index_x, index_y]      # récupération de la position des portes dans une liste
+                    portes.append(porte)
                 elif y == "O":                      # récupération de la position des murs dans une liste
                     mur = [index_x, index_y]
-                    self.obstacles.append(mur)   
+                    obstacles.append(mur)   
                 else:
                     pass
-                
                    
 
     def __repr__(self):   # méthode d'affichage de la carte
@@ -45,7 +47,9 @@ class Robot:
             self.mapp += '\n'
             for x in i:
                 self.mapp += x
-        return f"carte en jeu {self.mapp}"
+        return f"{self.mapp}"
+
+        
 
     def deplacer(self, direction):   # méthode pour les déplacements du robot
         entries = ["N", "S", "E", "O"]
@@ -53,10 +57,15 @@ class Robot:
         if direction not in entries:
             print("Direction inconnue")
         elif direction == "N":
-                self.robot[index_y] -= 1
+                self.robot[index_y] -= 1    
+                print(self.robot)            
         elif direction == "S":
                 self.robot[index_y] += 1
+                print(self.robot)         
         elif direction == "E":
                 self.robot[index_x] += 1
+                print(self.robot)         
         elif direction == "O":
                 self.robot[index_x] -= 1
+                print(self.robot)         
+        return self.mapp
