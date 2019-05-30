a3a3e44d2c28bcd0ea05d34035dfc289c22a7ba2
athd33 <antoinecours@gmx.fr>
2019-05-30 14:23:30 +0200

    a finir

--- roboc.py
+++ roboc.py
@@ -1,6 +1,7 @@
 import os
 import time
 from carte import *
+import os
 
 class Robot:
     """
@@ -21,22 +22,47 @@ class Robot:
         for i in grille:   # pour tous les elements de self.grille
             self.grilleToList.append(''.join(i)) # remplissage de mappToDisplay avec les elements sans espace    
         self.mappToDisplay = "\n".join(self.grilleToList)
+        os.system("cls")
+        os.system("clear")
         return f"{self.mappToDisplay}"
 
-
-
     def deplacer(self, direction):   # méthode pour les déplacements du robot
+        """
+        Méthode qui récupère le choix de direction de l'utilisateur et modifie en fonction, les éléments de la grille
+        """
         entries = ["N", "S", "E", "O"]
-        robot = self.labyrinthe_Online.robot
         self.grille = self.labyrinthe_Online.grille
+        robot = self.labyrinthe_Online.robot
         mapp = self.labyrinthe_Online.mappToDisplay
         Rx = robot[0]
         Ry = robot[1]
 
-        if direction == "N":
+        if direction == "O":
             if self.grille[Rx][Ry -1] == "O":
-                print("mur")
+                pass
             else:
+                self.grille[Rx][Ry] = " "
+                self.grille[Rx][Ry -1] = "X" 
                 robot[1] -= 1
-                self.grille[Rx][Ry] = "X"
-                self.grille[Rx][Ry +1] = " " 
+        
+        if direction == "E":
+            if self.grille[Rx][Ry +1] == "O":
+                pass
+            else:
+                self.grille[Rx][Ry] = " "
+                self.grille[Rx][Ry +1] = "X" 
+                robot[1] += 1
+        if direction == "N":
+            if self.grille[Rx -1][Ry] == "O":
+                pass
+            else:
+                self.grille[Rx][Ry] = " "
+                self.grille[Rx -1][Ry] = "X" 
+                robot[0] -= 1
+        if direction == "S":
+            if self.grille[Rx +1][Ry] == "O":
+                pass
+            else:
+                self.grille[Rx][Ry] = " "
+                self.grille[Rx +1][Ry] = "X" 
+                robot[0] += 1
